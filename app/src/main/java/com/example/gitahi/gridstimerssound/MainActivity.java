9d4525d968c295d58b89f4a7fcaf4d041ae86aa2
clintgitahi <clinton@yumkenya.com>
2019-05-22 12:05:54 +0300

    using random numbers and setting answers on buttons

--- app/src/main/java/com/example/gitahi/gridstimerssound/MainActivity.java
+++ app/src/main/java/com/example/gitahi/gridstimerssound/MainActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button goButton;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
 
     public void start(View view) {
         goButton.setVisibility(View.INVISIBLE);
@@ -17,6 +22,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView sumTextView = findViewById(R.id.sumTextView);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
         goButton = findViewById(R.id.goButton);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + "+ Integer.toString(b));
+
+        // where the correct answer should be located
+        int locationOfCorrectAnswer = rand.nextInt(4);
+
+        for(int i=0; i < 4; i++) {
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a+b);
+            } else {
+                int wrongAnswer = rand.nextInt(41);
+
+                while (wrongAnswer == a+b) {
+                    wrongAnswer = rand.nextInt(41);
+                }
+
+                answers.add(wrongAnswer);
+            }
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 }
