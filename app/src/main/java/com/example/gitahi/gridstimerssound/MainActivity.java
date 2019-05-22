61ec7ecfc2c31dc3e4862f2881d9bc53a93c7fb5
clintgitahi <clinton@yumkenya.com>
2019-05-22 10:35:12 +0300

    setting up views

---
+++ app/src/main/java/com/example/gitahi/gridstimerssound/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.gitahi.gridstimerssound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button goButton;
+
+    public void start(View view) {
+        goButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.goButton);
+    }
+}
